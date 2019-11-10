6a5f3dc4042f49936a8aa09331a90e1626c8c8b7
sergeblr <sergeblr@mail.ru>
2019-11-11 00:57:44 +0300

    jpa: profiles implemented, todo: populate DB at mysql & postgre mode

--- rest-app/src/main/java/com/cafemenu/restapp/RestApplication.java
+++ rest-app/src/main/java/com/cafemenu/restapp/RestApplication.java
@@ -4,12 +4,15 @@ import com.cafemenu.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan(basePackages = {"com.cafemenu"})
-@SpringBootApplication
+@EntityScan("com.cafemenu.entity")
+@SpringBootApplication      //(exclude = {DataSourceAutoConfiguration.class })
 @EnableJpaRepositories("com.cafemenu.repository")
 //@ImportResource(locations = {"classpath:test-db.xml"})
 public class RestApplication {
